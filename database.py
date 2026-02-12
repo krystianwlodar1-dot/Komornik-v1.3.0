dbb6043a8ab52c3e43059b583e0ad889e062fce4
krystianwlodar1-dot <krystian.wlodar1@gmail.com>
2026-02-12 10:55:57 +0100

    database.py

--- database.py
+++ database.py
@@ -1,34 +1,25 @@
 import sqlite3
-import threading
+from threading import Lock
 
+lock = Lock()
 conn = sqlite3.connect("houses.db", check_same_thread=False)
-lock = threading.Lock()
 c = conn.cursor()
 c.execute("""
 CREATE TABLE IF NOT EXISTS houses (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     map TEXT,
-    owner TEXT,
     status TEXT,
-    size TEXT,
-    player TEXT,
+    size INTEGER,
+    owner TEXT,
     last_login TEXT
 )
 """)
 conn.commit()
 
-def clear():
+def add(data):
     with lock:
-        c.execute("DELETE FROM houses")
-        conn.commit()
-
-def add(name, map_img, owner, status, size, player, last_login):
-    with lock:
-        c.execute(
-            "INSERT INTO houses (name, map, owner, status, size, player, last_login) VALUES (?, ?, ?, ?, ?, ?, ?)",
-            (name, map_img, owner, status, size, player, last_login)
-        )
+        c.execute("INSERT INTO houses (name,map,status,size,owner,last_login) VALUES (?,?,?,?,?,?)", data)
         conn.commit()
 
 def get_all():
@@ -40,3 +31,8 @@ def count_houses():
     with lock:
         c.execute("SELECT COUNT(*) FROM houses")
         return c.fetchone()[0]
+
+def clear():
+    with lock:
+        c.execute("DELETE FROM houses")
+        conn.commit()
